22eba850cbb0cd65ea2f97802348e8e0e74a8dda
Shruti <shrutishastri49@gmail.com>
2019-09-22 18:00:20 +0530

    changed the api link

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ window.addEventListener('load', () => {
 async function fetchData(late, longe) {
   try {
     const response = await fetch(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${late}&lon=${longe}&&units=metric&APPID=63c527087993d3c6b61373aad19465ba`
+      `https://api.openweathermap.org/data/2.5/weather?lat=${late}&lon=${longe}&&units=metric&APPID=63c527087993d3c6b61373aad19465ba`
     );
     const data = await response.json();
     displayData(data);
@@ -32,7 +32,7 @@ function displayData(result) {
   let img = document.getElementById('icon');
   img.setAttribute(
     'src',
-    `http://api.openweathermap.org/img/w/${result.weather[0].icon}.png`
+    `https://api.openweathermap.org/img/w/${result.weather[0].icon}.png`
   );
   document.getElementById('temp').textContent = Math.floor(result.main.temp);
   document.getElementById('desc').textContent = result.weather[0].description;
