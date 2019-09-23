b81b8b2a35269924c6c2dac61b6992ef8318a1c6
Shruti <shrutishastri49@gmail.com>
2019-09-23 12:36:04 +0530

    added background according to day and night

--- app.js
+++ app.js
@@ -8,8 +8,9 @@ window.addEventListener('load', () => {
     navigator.geolocation.getCurrentPosition(position => {
       lat = position.coords.latitude;
       long = position.coords.longitude;
-      console.log(position);
+
       preloader.classList.add('preload-finish');
+      changeBackground();
       fetchData(lat, long);
     });
   }
@@ -28,22 +29,61 @@ async function fetchData(late, longe) {
 }
 
 function displayData(result) {
-  console.log(result);
-  let img = document.getElementById('icon');
+  let windSpeed = document.querySelector('.wind-speed__value');
+  let humidity = document.querySelector('.humidity__value');
+  let cloudiness = document.querySelector('.cloudiness__value');
+  let temp = document.querySelector('.temperature__value');
+  let min = document.querySelector('.min-temp');
+  let max = document.querySelector('.max-temp');
+  let desc = document.querySelector('.weather__description');
+  let city = document.querySelector('.location__city');
+  let country = document.querySelector('.location__country--code');
+  let img = document.querySelector('.weather__icon');
+
+  humidity.textContent = result.main.humidity + ' %';
+  windSpeed.textContent = result.wind.speed + ' m/s';
+  cloudiness.textContent = result.clouds.all + ' %';
+  temp.textContent = Math.floor(result.main.temp);
+  min.textContent = Math.floor(result.main.temp_min);
+  max.textContent = Math.floor(result.main.temp_max);
+  desc.textContent = result.weather[0].description;
+  country.textContent = ',' + ' ' + result.sys.country;
+  city.textContent = result.name;
   img.setAttribute(
     'src',
     `https://api.openweathermap.org/img/w/${result.weather[0].icon}.png`
   );
-  document.getElementById('temp').textContent = Math.floor(result.main.temp);
-  document.getElementById('desc').textContent = result.weather[0].description;
-  document.querySelector('.header__location--city').textContent =
-    result.sys.name;
-  document.querySelector('.header__location--country').textContent =
-    result.name;
-  document.getElementById('pressure').textContent =
-    result.main.pressure + ' hpa';
-  document.getElementById('humidity').textContent = result.main.humidity + ' %';
-  document.getElementById('wind').textContent = result.wind.speed + ' m/s';
+
   let date = new Date();
   document.querySelector('.header__timezone').textContent = date.toDateString();
 }
+
+function changeBackground() {
+  let box = document.querySelector('.app__container');
+  let currentTime = new Date().getHours();
+  console.log(currentTime);
+  if (1 <= currentTime && currentTime < 20) {
+    box.classList.add('app__night');
+    document.body.style.backgroundColor = '#5e4b7f';
+    box.classList.remove('app__day');
+  } else {
+    box.classList.add('app__day');
+    document.body.style.backgroundColor = '#4f93a0';
+    box.classList.remove('app__night');
+  }
+}
+
+function changeTemperatureScale() {
+  let scale = document.querySelector('.scale');
+  console.log(scale);
+  console.log(scale.innerHTML);
+  scale.addEventListener('click', () => {
+    if (scale.innerHTML === '&#176;C') {
+      console.log(scale.innerHTML);
+      scale.innerHTML = '&#176;F';
+    } else {
+      scale.innerHTML = '&#176;C';
+    }
+  });
+}
+changeTemperatureScale();
