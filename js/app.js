0d2e187fa21e95b3de4504d38678280837484875
Shruti <shrutishastri49@gmail.com>
2019-09-24 14:15:44 +0530

    added geolocation permission

--- js/app.js
+++ js/app.js
@@ -1,3 +1,47 @@
+
+function handlePermission() {
+  navigator.permissions.query({ name: 'geolocation' }).then(function(result) {
+    if (result.state == 'granted') {
+      report(result.state);
+      geoBtn.style.display = 'none';
+    } else if (result.state == 'prompt') {
+      report(result.state);
+      geoBtn.style.display = 'none';
+      navigator.geolocation.getCurrentPosition(
+        revealPosition,
+        positionDenied,
+        geoSettings
+      );
+    } else if (result.state == 'denied') {
+      report(result.state);
+      geoBtn.style.display = 'inline';
+    }
+    result.onchange = function() {
+      report(result.state);
+    };
+  });
+}
+
+function report(state) {
+  console.log('Permission ' + state);
+}
+
+handlePermission();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*********WINDOW LOADER AND LOCATION USING NAVIGATOR GEOLOCATION*********/
 window.addEventListener('load', () => {
   const preloader = document.querySelector('.preload');
